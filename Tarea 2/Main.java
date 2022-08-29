bbfddbd2640a49abf929ca191ab19bd4cec253f0
Chucho98 <90016282+Chucho98@users.noreply.github.com>
2022-08-29 10:46:18 -0500

    Update Main.java

--- Tarea 2/Main.java
+++ Tarea 2/Main.java
@@ -2,7 +2,7 @@ package arregloadt;
 
 public class Main {
     public static void main(String[] args) {
-        //Constructor
+        // Constructor
         ArregloADT lista = new ArregloADT(5);
         // Método getLongitud
         System.out.println("El tamaño del arreglo es de: "+ lista.getLongitud());
@@ -17,7 +17,6 @@ public class Main {
         System.out.println(lista.getElemento(0));
         System.out.println(lista.getElemento(2));
         System.out.println(lista.getElemento(4));
-        // Método toString
         // Método toString
         System.out.println(lista.toString());
     }
